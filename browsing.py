afc9838806f7fd1709ec6026a40d906901396c8c
giwa <ugw.gi.world@gmail.com>
2015-05-30 01:23:25 +0900

    add confile

--- browsing.py
+++ browsing.py
@@ -8,7 +8,8 @@ user = os.environ['MARIADB_ID']
 password = os.environ['MARIADB_PASS']
 browsing_dao = BrowsingMariaDao('localhost', user, password, 'interop2015')
 browsing_reconstruct = BrowsingReconstruct(browsing_dao)
-save_result_dao = SaveResultDao('/home/interop/2015/prod/browsing_history/wide.sqlite3')
+save_result_db = os.environ['NEGI_SAVE_RESULT_DB']
+save_result_dao = SaveResultDao(save_result_db)
 
 for result in save_result_dao.get_result():
     browsing_reconstruct.add_http_result(result)
