bae8206cfdffbedab139033830f039bc5775740e
giwa <ugw.gi.world@gmail.com>
2015-05-30 01:55:16 +0900

    add conf

--- browsing.py
+++ browsing.py
@@ -1,8 +1,26 @@
 import os
+import argparse
+import configparser
 
 from dao.save_result_dao import SaveResultDao
 from dao.browsing_maria_dao import BrowsingMariaDao
 from browsing_reconstruct import BrowsingReconstruct
+
+description = """\
+Extract Browsing history from NEGI(https://github.com/westlab/negi)
+"""
+
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('-c', '--conf',\
+                    type=str,\
+                    help='directory path to config file')
+args = parser.parse_args()
+print(args)
+
+config = configparser.ConfigParser()
+config.read(args.conf)
+print(config['negi']['db'])
+exit(0)
 
 user = os.environ['MARIADB_ID']
 password = os.environ['MARIADB_PASS']
